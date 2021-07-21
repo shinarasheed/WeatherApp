d1880ed68c1ccf1ccc3e38d67a8af566f39ae9af
shina <rasheedshinaopeyemi@gmail.com>
2021-07-21 10:42:48 +0100

    add weather icon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import icon from '../assets/img/weathericon.png';
 
 const Header = () => {
   return (
-    <section className="header">
-      <h1>
-        Weatherly
-        <span role="img" aria-label={'umbrella'}>
-          ☂️
-        </span>
-      </h1>
+    <section className="header d-flex justify-content-center">
+      <h1>Weatherly</h1>
+      <img
+        style={{ width: '50px', height: '50px', marginLeft: '10px' }}
+        src={icon}
+        alt="weather"
+      />
     </section>
   );
 };
