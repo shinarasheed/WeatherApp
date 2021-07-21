96cd1937a75c588f357b1a5bb7849528b6940429
shina <rasheedshinaopeyemi@gmail.com>
2021-07-21 09:34:03 +0100

    add heroku post build script

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
@@ -25,13 +26,11 @@ app.get('/', (req, res) => {
 
 app.use('/', router);
 
-// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
 }
 
